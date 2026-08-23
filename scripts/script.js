fb040165446b45d5a635ca5b8e60d9e49072b07f
João <joaolucassilvasimoes123@gmail.com>
2026-08-23 16:55:31 -0300

    First Commit

---
+++ scripts/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+
+    $(document).on("keydown", function (evento) {
+        const tecla = evento.key.toUpperCase();
+
+
+        const ehLetra = /^[A-Z]$/.test(tecla);
+        if (!ehLetra) return;
+
+        mostrarLetra(tecla);
+    });
+
+});
+
+
+
+function falarLetra(letra) {
+
+    if (!("speechSynthesis" in window)) return;
+
+    var sintetizador = new SpeechSynthesisUtterance(letra);
+
+    sintetizador.lang = "pt-BR";
+    sintetizador.rate = 0.9;
+    sintetizador.pitch = 1;
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(sintetizador);
+}
+
+
+
+
+function mostrarLetra(letra) {
+    // coloca a letra que foi digitada no topo da pagina
+    $("#nome-letra-atual").text(letra);
+
+    // coloquei um nome padrao nas imagem, "letraA.png"
+    //ela monta o caminha da pasta
+    //depois pega o modal que vai trocar de conteudo e muda o caminho o alt dele
+    $("#letra-imagem")
+        .attr("src", "img/letras/letra" + letra + ".png")
+        .attr("alt", "Letra " + letra);
+    $("#letra-slot").addClass("mostrar");
+
+    // Mostra a imagem do objeto correspondente
+    mostrarObjeto(letra);
+
+    // fala a letra em voz alta
+    falarLetra(letra);
+}
+
+function mostrarObjeto(letra) {
+    //deixei os nomes com padrao e faz a mesma coisa do mostra letra so que com os objetos 
+    //troca o caminho da pasta e coloca o alt diferente
+    //mesma coisa do de mostrar letra
+    const caminhoImagem = "img/objects/objeto" + letra + ".png";
+    
+    $("#modal-image")
+        .attr("src", caminhoImagem)
+        .attr("alt"," LETRA :" + letra)
+        .addClass("mostrar");
+}
